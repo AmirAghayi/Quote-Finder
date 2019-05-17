bc75ef19226c46d2368b4c31a84504f3b94b0c04
samskeen <sskeen9@gmail.com>
2019-05-16 20:07:09 -0600

    nav

--- src/Components/Frontheader/Frontheader.js
+++ src/Components/Frontheader/Frontheader.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Logo from './image/Logo.png';
+import './Frontheader.css';
 
 
 
@@ -10,8 +12,31 @@ class Frontheader extends Component {
 
 render (){
   return (
-    <div>
-         <h1>This is the header!</h1>
+    <div className="frontheader">
+         <section className="logo-nav">
+               <div className="logo">
+                    <img 
+                    className="Logo-icon"
+                    src={Logo}
+                    alt="Logo"
+                    />
+               </div>
+               <div className="nav">
+                     <ul>
+                       <li>Home</li>
+                       <li>About</li>
+                       <li>Register</li>
+                       <li>Login</li>
+                       <li>Contact us</li>
+                     </ul>
+               </div>
+
+         </section>
+
+
+         <section className="carousel">
+                
+         </section>
     </div>
     
     )
