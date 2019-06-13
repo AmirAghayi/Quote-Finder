b58c60846df6366eae5c8509aebcb0b309740e2d
Amir aghayi_chadegani <amiraghayi_chadegani@Amirs-iMac.local>
2019-06-13 02:58:05 -0600

    about-bugs-fixed-2

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -18,7 +18,7 @@ render (){
           <Navbar />
         
       
-          <p className="title">ABOUT US</p>
+          <p className="about-title">ABOUT US</p>
           <hr className="line"/>
           <p className="about-para">This website Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, deleniti doloribus dolore perspiciatis facere distinctio magnam deserunt eum unde numquam voluptatibus id consequatur laudantium iste architecto ullam nemo eius est! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus sunt a ab eaque neque aut dolore quam quisquam quia nobis, esse provident non! Provident sint, minus quas voluptate adipisci illo.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab quis sequi eveniet ex. Ipsum tempore laudantium enim, eaque deleniti eos quibusdam consequuntur reiciendis aspernatur cum saepe harum. Voluptatem, eveniet ex.</p>
           <Footer />
