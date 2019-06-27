19471d78f096f9d074234db6186d845b49711c53
Amir Aghayi <a_art_20@yahoo.com>
2019-06-26 19:23:44 -0600

    quote-cards-styling

--- src/Components/Frontbody/Frontbody.js
+++ src/Components/Frontbody/Frontbody.js
@@ -14,16 +14,20 @@ class Frontbody extends Component {
   render() {
     return this.props.quotes.map((quote) => (
 
-      <div className="quotes">
-        <Link 
-        to={`/quotes/${quote.id}`}
-        className="slide">
-          <h1 >"{quote.body}"</h1>
-          <div className="author">
-            <h2>{quote.author}</h2>
-          </div>
-        </Link>
+      <div className="quote-card-container">
+        <section className="quotes">
+          <Link
+            to={`/quotes/${quote.id}`}
+            className="slide">
+            <h1 >"{quote.body}"</h1>
+            <div className="author">
+              <h2>{quote.author}</h2>
+            </div>
+          </Link>
+        </section>
       </div>
+
+
 
     ))
 
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -9,42 +9,51 @@ import './Homepage.css';
 
 
 class Homepage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-       quotesList: []
+      quotesList: []
     };
   }
 
 
 
-componentDidMount(){
-  this.getQuotes();
-}
+  componentDidMount() {
+    this.getQuotes();
+  }
 
 
-getQuotes = () => {
-   axios.get('https://favqs.com/api/quotes', { headers: {Authorization: `Token token="7e1cd958d0d8cfebace9c3d0e5c146e9"`}})
-   .then(response => {
-     console.log(response)
-     this.setState({
-       quotesList: response.data.quotes
-     })
-   })
-}
+  getQuotes = () => {
+    axios.get('https://favqs.com/api/quotes', { headers: { Authorization: `Token token="7e1cd958d0d8cfebace9c3d0e5c146e9"` } })
+      .then(response => {
+        console.log(response)
+        this.setState({
+          quotesList: response.data.quotes
+        })
+      })
+  }
 
 
-render (){
-  
+  render() {
 
-  return (
-    <div className="Homepage" style={{height: '100%'}}>
-         <CarouselView />
-         <Frontbody className="quote-card-container" quotes={this.state.quotesList}/>
-         <Footer />
-    </div>
-    
+
+    return (
+      <div className="Homepage" style={{ height: '100%' }}>
+        <section className="carousel-container">
+          <CarouselView />
+        </section>
+
+        <section className="quote-cards-container">
+          <Frontbody className="quote-card-container" quotes={this.state.quotesList} />
+        </section>
+
+        <div className="footer-container">
+          <Footer />
+        </div>
+
+      </div>
+
     )
   }
 
